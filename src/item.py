ea564898d6923f7bff334b7e0618ca866f2ccd02
petr-fol <144835428+petr-fol@users.noreply.github.com>
2023-10-13 20:23:39 +0600

    update project

--- src/item.py
+++ src/item.py
@@ -2,10 +2,10 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate_ = 1.0
+    pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int, pay_rate=1.0) -> None:
+    def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
@@ -16,19 +16,18 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
-        self.pay_rate = pay_rate
+        # self.pay_rate = pay_rate
 
-    def calculate_total_price(self, pay_rate_) -> float:
+    def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
         :return: Общая стоимость товара.
         """
-        self.pay_rate = pay_rate_
-        return self.price * self.pay_rate
+        return self.price * Item.pay_rate
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = self.price * self.pay_rate
+        self.price = self.price * Item.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -8,18 +8,18 @@ def test_args():
     assert test_item.name == "name"
     assert test_item.price == 1.0
     assert test_item.quantity == 1
-    price = test_item.calculate_total_price(1.5)
-    assert test_item.pay_rate == 1.5
-    assert price == 1.5
+    price = test_item.calculate_total_price()
+    assert test_item.pay_rate == 1.0
+    assert price == 1.0
 
 
 def test_calculate_total_price():
-    price = test_item.calculate_total_price(1.2)
-    assert price == 1.2
+    price = test_item.calculate_total_price()
+    assert price == 1.0
     assert type(price) == float
 
 
 def test_apply_discount():
     test_item.apply_discount()
-    assert test_item.price == 1.2
+    assert test_item.price == 1.0
 
