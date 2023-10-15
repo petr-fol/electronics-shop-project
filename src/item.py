62e57686d5eee72add3a3dc1e6a48de16c84898d
petr-fol <144835428+petr-fol@users.noreply.github.com>
2023-10-16 01:59:15 +0600

    update item.py

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         # self.pay_rate = pay_rate
@@ -32,3 +35,40 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if 0 < len(new_name) < 10:
+            self.__name = new_name
+        else:
+            self.__name = new_name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, shell):
+        with open(shell) as csv_f:
+            csv_object = csv.DictReader(csv_f)
+            list_items = []
+            for row in csv_object:
+                name = row['name']
+                price = row['price']
+                quantity = row['quantity']
+                item = cls(name, float(price), int(quantity))
+                list_items.append(item)
+        return list_items
+
+    @staticmethod
+    def string_to_number(num_str):
+        if "." in num_str:
+            int_num = ''
+            for i in num_str:
+                if i != ".":
+                   int_num += i
+                else:
+                    break
+            return int(int_num)
+        else:
+            return int(num_str)
