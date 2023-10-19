9ff7cba41a4530dbc4480e888c27f02f124000ce
petr-fol <144835428+petr-fol@users.noreply.github.com>
2023-10-20 01:48:00 +0600

    remove one space item.py

--- src/item.py
+++ src/item.py
@@ -94,7 +94,7 @@ class Item:
         return int(num_str)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price},  {self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
     def __str__(self):
         return self.name
