dd6e10bb2b24ec1c03a203512cc478f22d725ece
petr-fol <144835428+petr-fol@users.noreply.github.com>
2023-10-21 18:46:48 +0600

    fix: try, except for sim_count

--- src/phone.py
+++ src/phone.py
@@ -4,11 +4,9 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, sim_count: int) -> None:
         super().__init__(name, price, quantity)
-        try:
-            if sim_count > 0:
-                self.sim_count = sim_count
-        except ValueError:
-            "не может быть 0 сим карт!"
+        if sim_count <= 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self.sim_count = sim_count
 
     def __str__(self):
         return self.name
