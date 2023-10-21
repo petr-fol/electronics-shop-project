ba32c8c441e0f23ba2901f710a2622fe0a876f35
petr-fol <144835428+petr-fol@users.noreply.github.com>
2023-10-21 18:55:30 +0600

    fix: conflicts

--- src/item.py
+++ src/item.py
@@ -94,10 +94,10 @@ class Item:
         return int(num_str)
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(name='{self.name}', price={self.price}, quantity={self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
     def __str__(self) -> str:
-        return f"Item: {self.name}, Price: {self.price}, Quantity: {self.quantity}"
+        return self.name
 
     def __add__(self, other) -> int:
         if isinstance(other, Item):
