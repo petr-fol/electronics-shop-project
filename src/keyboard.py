0be8d3e63efd67f43611bf5f8368910418b925df
petr-fol <144835428+petr-fol@users.noreply.github.com>
2023-10-25 03:01:38 +0600

    add KeyboardMixin, Keyboard, -classes

---
+++ src/keyboard.py
@@ -0,0 +1,28 @@
+from src.item import Item  # Подключаем базовый класс Item
+
+
+class KeyboardMixin:
+    def __init__(self, language):
+        self.language = language
+
+    @property
+    def language(self):
+        return self._language
+
+    @language.setter
+    def language(self, new_language):
+        if new_language in ['EN', 'RU']:
+            self._language = new_language
+
+    def change_lang(self):
+        if self.language == 'EN':
+            self.language = 'RU'
+        elif self.language == 'RU':
+            self.language = 'EN'
+
+
+class Keyboard(Item, KeyboardMixin):
+    def __init__(self, name, price, quantity, language='EN'):
+        super().__init__(name, price, quantity)
+        super(KeyboardMixin, self).__init__()
+        self.language = language
